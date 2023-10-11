98f96b7df4760d0ff88ea0504ea47fd08378f2bc
abdulhay103 <abdulhay.me@gmail.com>
2023-10-11 13:35:00 +0600

    in working

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main className=" container mx-auto pt-20">
+            <h1>Module-18 Assignment</h1>
+        </main>
+    );
+}
